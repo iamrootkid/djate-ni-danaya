8a3dd18db750c86b0f30de59f4151a71f55e67a5
iamrootkid <iamrootkid@gmail.com>
2025-05-14 00:27:30 +0530

    Réinitialisation du dépôt avec la version locale

--- modify_categories_constraint.sql
+++
@@ -1,4 +0,0 @@
-
--- Drop existing name unique constraint and add composite constraint with shop_id
-ALTER TABLE categories DROP CONSTRAINT IF EXISTS categories_name_key;
-ALTER TABLE categories ADD CONSTRAINT categories_name_shop_id_key UNIQUE (name, shop_id);
--- modify_settings_constraint.sql
+++
@@ -1,2 +0,0 @@
-
-ALTER TABLE settings DROP CONSTRAINT IF EXISTS settings_key_key; ALTER TABLE settings ADD CONSTRAINT settings_key_shop_id_key UNIQUE (key, shop_id);
